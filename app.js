a56697fde1a898d19001c3ce8c1a8051648f5ab3
Harshita Arora <110261198+HARSHITARora12@users.noreply.github.com>
2024-03-06 12:59:55 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+document.getElementById('clear-btn').addEventListener('click', function() {
+    document.getElementById('fact-input').value = '';
+});
+
+
+
+document.getElementById('submit-btn').addEventListener('click', function () {
+    // Redirect to another page
+    window.location.href = 'output.html'; // Change 'output.html' to the desired URL
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var profileIcon = document.getElementById('user-profile');
+    var profileTextarea = document.getElementById('profile-textarea');
+
+    profileIcon.addEventListener('click', function() {
+        // Toggle visibility of textarea
+        if (profileTextarea.style.display === 'none') {
+            profileTextarea.style.display = 'block';
+        } else {
+            profileTextarea.style.display = 'none';
+        }
+    });
+});
+document.getElementById("profile-icon").addEventListener("click", function() {
+    document.getElementById("popup").style.display = "block";
+  });
+  
+  // Close the popup if clicked outside
+  window.addEventListener("click", function(event) {
+    var popup = document.getElementById("popup");
+    if (event.target != popup && event.target.parentNode != popup) {
+      popup.style.display = "none";
+    }
+  });
---
+++ output.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Your JavaScript code here
+    document.getElementById('refresh-btn').addEventListener('click', function () {
+        // Redirect to the current page
+        window.location.href = 'fake_fact_detection.html';
+    });
+});
