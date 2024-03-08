ec1502448050930ee62fff5425564e22ac3ff25b
Harshita Arora <110261198+HARSHITARora12@users.noreply.github.com>
2024-03-08 22:31:54 +0530

    Add files via upload

---
+++ output.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    document.getElementById('refresh-btn').addEventListener('click', function () {
+      
+        window.location.href = 'fake_fact_detection.html';
+    });
+});
