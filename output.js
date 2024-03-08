72c1755eb20a10cb52c429ce845c94bc360632e8
Harshita Arora <110261198+HARSHITARora12@users.noreply.github.com>
2024-03-08 22:26:35 +0530

    Delete output.js

--- output.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Your JavaScript code here
-    document.getElementById('refresh-btn').addEventListener('click', function () {
-        // Redirect to the current page
-        window.location.href = 'fake_fact_detection.html';
-    });
-});
